41c41e1bd10e5fbc26693b1225ae2a6af68e51fc
Fawzi Gharib <fawz11gharib@gmail.com>
2023-09-07 17:50:12 +0300

    Adding Google Font & Font Family

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:bookly/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'features/splash/presentation/views/splash_view.dart';
 
 void main() {
@@ -19,6 +20,8 @@ class BooklyRoot extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         theme: ThemeData.dark().copyWith(
           scaffoldBackgroundColor: kPrimaryColor,
+          textTheme:
+              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
         ),
         home: const SplashView(),
       ),
