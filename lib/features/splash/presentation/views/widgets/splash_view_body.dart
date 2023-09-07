6f3c29595863c6c09b2714a0a643d53d0d82e48b
Fawzi Gharib <fawz11gharib@gmail.com>
2023-09-07 14:35:01 +0300

    Sliding Animation | Splash Body

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -2,8 +2,44 @@ import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<Offset> slideAnimationFromTop;
+  late Animation<Offset> slideAnimationFromBottom;
+  late Animation<Opacity> opacityAnimation;
+
+  @override
+  void initState() {
+    controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+
+    slideAnimationFromTop = Tween<Offset>(
+      begin: const Offset(0, -2),
+      end: Offset.zero,
+    ).animate(controller);
+    slideAnimationFromBottom =
+        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
+            .animate(controller);
+
+    super.initState();
+
+    controller.forward();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    controller.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,18 +47,54 @@ class SplashViewBody extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Image.asset(
-            Assets.logo,
-            width: 200.w,
+          SlidingAnimation(
+            slideAnimationFromBottom: slideAnimationFromTop,
+            controller: controller,
+            child: Image.asset(
+              Assets.logo,
+              width: 200.w,
+            ),
           ),
           SizedBox(
             height: 16.h,
           ),
-          const Text(
-            'Read Free Books',
-            textAlign: TextAlign.center,
-          ),
+          SlidingAnimation(
+              slideAnimationFromBottom: slideAnimationFromBottom,
+              controller: controller,
+              child: const Text(
+                'Read Free Books',
+                textAlign: TextAlign.center,
+              )),
         ],
+      ),
+    );
+  }
+}
+
+class SlidingAnimation extends StatelessWidget {
+  const SlidingAnimation({
+    super.key,
+    required this.slideAnimationFromBottom,
+    required this.child,
+    required this.controller,
+  });
+
+  final Animation<Offset> slideAnimationFromBottom;
+  final Widget child;
+  final AnimationController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: slideAnimationFromBottom,
+      builder: (context, _) => SlideTransition(
+        position: slideAnimationFromBottom,
+        child: AnimatedOpacity(
+          opacity: controller.value,
+          curve: Curves.decelerate,
+          duration: const Duration(seconds: 1),
+          child: child,
+        ),
       ),
     );
   }
